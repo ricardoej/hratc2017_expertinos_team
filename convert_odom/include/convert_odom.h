4b5b480a3d83c40b554189f0fa7bb6a876052197
Audeliano <audeliano@yahoo.com.br>
2017-03-12 19:38:09 -0300

    testing slam

--- convert_odom/include/convert_odom.h
+++ convert_odom/include/convert_odom.h
@@ -4,6 +4,7 @@
 #include <ros/ros.h>
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <geometry_msgs/PoseStamped.h>
 
 
 class ConvertOdom
@@ -15,6 +16,8 @@ public:
 public:
 
   void ekfOdomCallback(const geometry_msgs::PoseWithCovarianceStampedConstPtr& ekf_odom);
+  void slamPoseCallback(const geometry_msgs::PoseStampedConstPtr& slam_pose);
+
   void spin();
 
 private:
@@ -22,9 +25,13 @@ private:
   ros::NodeHandle n_;
 
   ros::Subscriber ekf_odom_sub_;
+  ros::Subscriber slam_pose_sub_;
+
   ros::Publisher convertOdom_pub_;
+  ros::Publisher convertSlamPose_pub_;
 
   nav_msgs::Odometry odom_converted_;
+  nav_msgs::Odometry slam_pose_converted_;
 
 
 };
--- convert_odom/src/convert_odom.cpp
+++ convert_odom/src/convert_odom.cpp
@@ -6,13 +6,18 @@ ConvertOdom::ConvertOdom(ros::NodeHandle n)
 {
 
   ekf_odom_sub_ = n_.subscribe("robot_pose_ekf/odom",100, &ConvertOdom::ekfOdomCallback, this);
+  slam_pose_sub_ = n_.subscribe("slam_out_pose",100, &ConvertOdom::slamPoseCallback, this);
+
   convertOdom_pub_ = n_.advertise<nav_msgs::Odometry>("odom_w_ekf",100);
+  convertSlamPose_pub_ = n_.advertise<nav_msgs::Odometry>("slam_out_odom",100);
 
 }
 
 ConvertOdom::~ConvertOdom(){
   ekf_odom_sub_.shutdown();
+  slam_pose_sub_.shutdown();
   convertOdom_pub_.shutdown();
+  convertSlamPose_pub_.shutdown();
 }
 
 void ConvertOdom::ekfOdomCallback(const geometry_msgs::PoseWithCovarianceStampedConstPtr& ekf_odom)
@@ -29,6 +34,22 @@ void ConvertOdom::ekfOdomCallback(const geometry_msgs::PoseWithCovarianceStamped
   odom_converted_.pose.pose.orientation.z = ekf_odom->pose.pose.orientation.z;
 }
 
+void ConvertOdom::slamPoseCallback(const geometry_msgs::PoseStampedConstPtr& slam_pose)
+{
+  slam_pose_converted_.header.frame_id = slam_pose->header.frame_id;
+  slam_pose_converted_.header.stamp = slam_pose->header.stamp;
+
+  slam_pose_converted_.pose.pose.position.x = slam_pose->pose.position.x;
+  slam_pose_converted_.pose.pose.position.y = slam_pose->pose.position.y;
+  slam_pose_converted_.pose.pose.position.z = slam_pose->pose.position.z;
+
+  slam_pose_converted_.pose.pose.orientation.x = slam_pose->pose.orientation.x;
+  slam_pose_converted_.pose.pose.orientation.y = slam_pose->pose.orientation.y;
+  slam_pose_converted_.pose.pose.orientation.z = slam_pose->pose.orientation.z;
+  slam_pose_converted_.pose.pose.orientation.w = slam_pose->pose.orientation.w;
+
+}
+
 void ConvertOdom::spin()
 {
   ros::Rate loopRate(30);
@@ -36,6 +57,7 @@ void ConvertOdom::spin()
   {
 
     convertOdom_pub_.publish(odom_converted_);
+    convertSlamPose_pub_.publish(slam_pose_converted_);
 
     ros::spinOnce();
     loopRate.sleep();
