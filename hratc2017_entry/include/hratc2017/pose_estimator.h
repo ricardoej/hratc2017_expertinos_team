d9c03e29fdba78bdbfe9932bbc5af7482510b7fd
luizfernandolfn <luizfernandolfn@gmail.com>
2017-03-24 13:11:55 -0300

    created pose_estimator

---
+++ hratc2017_entry/include/hratc2017/pose_estimator.h
@@ -0,0 +1,73 @@
+/**
+ *  This source file implements the MapController class, which is
+ *based on the ROSNode helper class. It controls the map_Controller_node.
+ *
+ *  Version: 1.0.0
+ *  Created on: 21/03/2017
+ *  Modified on: 21/03/2017
+ *  Author: Ricardo Emerson Julio (ricardoej@gmail.com)
+ *  Maintainer: Expertinos UNIFEI (expertinos.unifei@gmail.com)
+ */
+
+#ifndef _HRATC2017_ENTRIES_MAP_CONTROLLER_H_
+#define _HRATC2017_ENTRIES_MAP_CONTROLLER_H_
+
+#include <ros/ros.h>
+#include "utilities/ros_node.h"
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <visualization_msgs/MarkerArray.h>
+
+#define CENTER_X 483238.6184840562
+#define CENTER_Y 6674530.097688958
+
+
+namespace hratc2017
+{
+
+namespace states{
+
+enum StateEnum
+{
+  S1_READING1,
+  S2_MOVING,
+  S3_READING2,
+};
+}
+
+typedef states::StateEnum StateEnum;
+
+class PoseEstimator : public utilities::ROSNode
+{
+public:
+  PoseEstimator(ros::NodeHandle* nh);
+  virtual ~PoseEstimator();
+
+private:
+  bool initial_pose_sent_;
+  bool has_utm_reading1_;
+  bool has_utm_reading2_;
+  bool isMoving_;
+  geometry_msgs::PoseWithCovarianceStamped p1_;
+  geometry_msgs::PoseWithCovarianceStamped p2_;
+  nav_msgs::Odometry utm_reading1_;
+  nav_msgs::Odometry utm_reading2_;
+  nav_msgs::Odometry odom_p3at_;
+  ros::Subscriber gps_odom_sub_;
+  ros::Subscriber odom_p3at_sub_;
+  ros::Publisher cmd_vel_pub_;
+  ros::Publisher initial_orientation_pub_;
+  double centerX_;
+  double centerY_;
+  StateEnum current_state_;
+  virtual void controlLoop();
+  void gpsOdomCallback(const nav_msgs::Odometry::ConstPtr& msg);
+  void odomP3atCallback(const nav_msgs::Odometry::ConstPtr &msg);
+  void cornersCallback(const visualization_msgs::MarkerArray::ConstPtr& msg);
+  void setNextStage();
+  void setVelocity(double vx, double wz);
+
+};
+}
+
+#endif /* _HRATC2017_ENTRIES_MAP_CONTROLLER_H_ */
---
+++ hratc2017_entry/src/hratc2017/pose_estimator.cpp
@@ -0,0 +1,123 @@
+/**
+ *  This source file implements the MapController class, which is
+ *based on the ROSNode helper class. It controls the map_Controller_node.
+ *
+ *  Version: 1.0.0
+ *  Created on: 21/03/2017
+ *  Modified on: 21/03/2017
+ *  Author: Ricardo Emerson Julio (ricardoej@gmail.com)
+ *  Maintainer: Expertinos UNIFEI (expertinos.unifei@gmail.com)
+ */
+
+#include "hratc2017/pose_estimator.h"
+
+namespace hratc2017
+{
+
+/**
+ * @brief MapController::MapController
+ * @param nh
+ */
+PoseEstimator::PoseEstimator(ros::NodeHandle* nh)
+  : ROSNode(nh, 30), initial_pose_sent_(false), current_state_(states::S1_READING1),
+    has_utm_reading1_(false), has_utm_reading2_(false), isMoving_(false)
+{
+  ros::NodeHandle pnh("~");
+  pnh.param("centerX", centerX_, CENTER_X);
+  ROS_INFO("   center X: %f", centerX_);
+  pnh.param("centerY", centerY_, CENTER_Y);
+  ROS_INFO("   center Y: %f", centerY_);
+
+  gps_odom_sub_ = nh->subscribe("/gps/odom", 100, &PoseEstimator::gpsOdomCallback, this);
+  odom_p3at_sub_ = nh->subscribe("/p3at/odom", 100, &PoseEstimator::odomP3atCallback, this);
+  initial_orientation_pub_ = nh->advertise<geometry_msgs::PoseWithCovarianceStamped>("/initialOrietation", 100, true);
+  cmd_vel_pub_ = nh->advertise<geometry_msgs::Twist>("cmd_vel", 100, true);
+}
+
+/**
+ * @brief MapController::~MapController
+ */
+PoseEstimator::~PoseEstimator()
+{
+  gps_odom_sub_.shutdown();
+  cmd_vel_pub_.shutdown();
+  initial_orientation_pub_.shutdown();
+  odom_p3at_sub_.shutdown();
+}
+
+/**
+ * @brief MapController::controlLoop
+ */
+void PoseEstimator::controlLoop()
+{ 
+  if (!initial_pose_sent_ && current_state_!=states::S2_MOVING)
+  {
+    ROS_INFO("READINGS...");
+    if(has_utm_reading1_ && !has_utm_reading2_){
+      ROS_INFO("READING 1");
+      p1_.pose = utm_reading1_.pose;
+      p1_.pose.pose.position.x = utm_reading1_.pose.pose.position.x - centerX_;
+      p1_.pose.pose.position.y = utm_reading1_.pose.pose.position.y - centerY_;
+    }
+    else if(has_utm_reading2_){
+      ROS_INFO("READING 2");
+      p2_.pose = utm_reading2_.pose;
+      p2_.pose.pose.position.x = utm_reading1_.pose.pose.position.x - centerX_;
+      p2_.pose.pose.position.y = utm_reading1_.pose.pose.position.y - centerY_;
+      p2_.pose.pose.position.z = atan((p2_.pose.pose.position.y - p1_.pose.pose.position.y) / (p2_.pose.pose.position.x - p1_.pose.pose.position.x));
+      initial_orientation_pub_.publish(p2_);
+      initial_pose_sent_ = true;
+      ROS_INFO("Published initial pose (x=%f, y=%f, z=%f)", p2_.pose.pose.position.x, p2_.pose.pose.position.y, p2_.pose.pose.position.z);
+    }
+  }
+}
+
+/**
+ * @brief MapController::gpsOdomCallback receives gps odom from robot
+ * @param msg gps odom
+ */
+void PoseEstimator::gpsOdomCallback(const nav_msgs::Odometry::ConstPtr& msg)
+{
+  switch (current_state_) {
+  case states::S1_READING1:
+    ROS_INFO("Started state: reading 1");
+    utm_reading1_ = *msg;
+    has_utm_reading1_ = true;
+    current_state_ = states::S2_MOVING;
+    break;
+  case states::S2_MOVING:
+    ROS_INFO("Started state: moving");
+    if(!isMoving_)
+    {
+      setVelocity(0.5, 0);
+      isMoving_ = true;
+    }
+    if(odom_p3at_.pose.pose.position.x > 1){
+      setVelocity(0, 0);
+      current_state_ = states::S3_READING2;
+    }
+    break;
+  case states::S3_READING2:
+    ROS_INFO("Started state: reading 1");
+    utm_reading2_ = *msg;
+    has_utm_reading2_ = true;
+    break;
+  }
+
+}
+
+void PoseEstimator::odomP3atCallback(const nav_msgs::Odometry::ConstPtr& msg){
+  odom_p3at_.pose.pose.position.x = msg->pose.pose.position.x;
+
+}
+
+void PoseEstimator::setVelocity(double vx, double wz)
+{
+  geometry_msgs::Twist msg;
+  msg.linear.x = vx;
+  msg.angular.z = wz;
+  cmd_vel_pub_.publish(msg);
+}
+
+
+}
