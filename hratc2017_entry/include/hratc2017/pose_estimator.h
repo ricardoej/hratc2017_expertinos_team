f23386b7400ef29cfbe6c060c4eb148fd52f512e
luizfernandolfn <luizfernandolfn@gmail.com>
2017-03-24 20:01:10 -0300

    fixed pose_estimated

--- hratc2017_entry/include/hratc2017/pose_estimator.h
+++ hratc2017_entry/include/hratc2017/pose_estimator.h
@@ -16,7 +16,11 @@
 #include "utilities/ros_node.h"
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <sensor_msgs/Imu.h>
 #include <visualization_msgs/MarkerArray.h>
+#include <utilities/mean_filter.h>
+#include "tf/tf.h"
+#include <tf/transform_datatypes.h>
 
 #define CENTER_X 483238.6184840562
 #define CENTER_Y 6674530.097688958
@@ -49,25 +53,45 @@ private:
   bool pose_estimated_sent_;
   bool has_utm_reading1_;
   bool has_utm_reading2_;
+  bool has_imu_initial_;
+  bool has_odom_initial_;
   bool isMoving_;
+  //ros::Time timer_;
+  //utilities::MeanFilter mean_filter_;
   geometry_msgs::PoseWithCovarianceStamped p1_;
   geometry_msgs::PoseWithCovarianceStamped p2_;
+  tf::Quaternion quat_initial_;
+  tf::Quaternion quat_data_;
+  tf::Quaternion quat_ekf_;
+  sensor_msgs::Imu imu_data_;
+  sensor_msgs::Imu imu_initial_;
+  sensor_msgs::Imu imu_ekf_;
   nav_msgs::Odometry utm_reading1_;
   nav_msgs::Odometry utm_reading2_;
   nav_msgs::Odometry odom_p3at_;
+  nav_msgs::Odometry odom_initial_;
   ros::Subscriber gps_odom_sub_;
   ros::Subscriber odom_p3at_sub_;
+  ros::Subscriber imu_sub_;
+  ros::Subscriber initial_pose_sub_;
+  ros::Publisher imu_pub_;
   ros::Publisher cmd_vel_pub_;
   ros::Publisher pose_estimated_pub_;
   double centerX_;
   double centerY_;
+  double yaw_initial_;
+  double yaw_data_;
+  double yaw_ekf_;
   StateEnum current_state_;
   virtual void controlLoop();
   void gpsOdomCallback(const nav_msgs::Odometry::ConstPtr& msg);
   void odomP3atCallback(const nav_msgs::Odometry::ConstPtr &msg);
   void cornersCallback(const visualization_msgs::MarkerArray::ConstPtr& msg);
+  void imuCallback(const sensor_msgs::Imu::ConstPtr& msg);
+  void initialPoseCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg);
   void setNextStage();
   void setVelocity(double vx, double wz);
+
 
 };
 }
--- hratc2017_entry/src/hratc2017/pose_estimator.cpp
+++ hratc2017_entry/src/hratc2017/pose_estimator.cpp
@@ -20,7 +20,7 @@ namespace hratc2017
  */
 PoseEstimator::PoseEstimator(ros::NodeHandle* nh)
   : ROSNode(nh, 30), pose_estimated_sent_(false), current_state_(states::S1_READING1),
-    has_utm_reading1_(false), has_utm_reading2_(false), isMoving_(false)
+    has_utm_reading1_(false), has_utm_reading2_(false), isMoving_(false), has_imu_initial_(false), has_odom_initial_(false)
 {
   ros::NodeHandle pnh("~");
   pnh.param("centerX", centerX_, CENTER_X);
@@ -30,8 +30,13 @@ PoseEstimator::PoseEstimator(ros::NodeHandle* nh)
 
   gps_odom_sub_ = nh->subscribe("/gps/odom", 100, &PoseEstimator::gpsOdomCallback, this);
   odom_p3at_sub_ = nh->subscribe("/p3at/odom", 100, &PoseEstimator::odomP3atCallback, this);
+  //odom_p3at_sub_ = nh->subscribe("/RosAria/pose", 100, &PoseEstimator::odomP3atCallback, this);
+  imu_sub_ = nh->subscribe("imu/data", 100, &PoseEstimator::imuCallback, this);
+  initial_pose_sub_ = nh->subscribe("initialpose", 100, &PoseEstimator::initialPoseCallback, this);
   pose_estimated_pub_ = nh->advertise<geometry_msgs::PoseWithCovarianceStamped>("/poseEstimated", 100, true);
   cmd_vel_pub_ = nh->advertise<geometry_msgs::Twist>("p3at/cmd_vel", 100, true);
+  //cmd_vel_pub_ = nh->advertise<geometry_msgs::Twist>("RosAria/cmd_vel", 100, true);
+  imu_pub_ = nh->advertise<sensor_msgs::Imu>("imu_w_offset", 100, true);
 }
 
 /**
@@ -65,13 +70,24 @@ void PoseEstimator::controlLoop()
       p2_.pose.pose.position.y = utm_reading2_.pose.pose.position.y - centerY_;
       current_state_ = states::S4_FINISHED;
       //get angle z by atan
-      p2_.pose.pose.position.z = atan((p2_.pose.pose.position.y - p1_.pose.pose.position.y) / (p2_.pose.pose.position.x - p1_.pose.pose.position.x));
+      p2_.pose.pose.position.z = atan2((p2_.pose.pose.position.y - p1_.pose.pose.position.y) , (p2_.pose.pose.position.x - p1_.pose.pose.position.x));
       pose_estimated_pub_.publish(p2_);
       pose_estimated_sent_ = true;
       ROS_INFO("p1 - (%f,%f)", p1_.pose.pose.position.x, p1_.pose.pose.position.y);
       ROS_INFO("p2 - (%f,%f)", p2_.pose.pose.position.x, p2_.pose.pose.position.y);
       ROS_INFO("Published pose estimated (x=%f, y=%f, z=%f)", p2_.pose.pose.position.x, p2_.pose.pose.position.y, p2_.pose.pose.position.z);
     }
+  }else{
+    yaw_ekf_ = yaw_data_ + p2_.pose.pose.position.z - yaw_initial_;
+    quat_ekf_ = tf::createQuaternionFromYaw(yaw_ekf_);
+
+    imu_ekf_ = imu_data_;
+    //imu_ekf_.orientation.x = quat_ekf_.getX();
+    //imu_ekf_.orientation.y = quat_ekf_.getY();
+    imu_ekf_.orientation.z = quat_ekf_.getZ();
+    imu_ekf_.orientation.w = quat_ekf_.getW();
+
+    imu_pub_.publish(imu_ekf_);
   }
 }
 
@@ -87,6 +103,13 @@ void PoseEstimator::gpsOdomCallback(const nav_msgs::Odometry::ConstPtr& msg)
     utm_reading1_ = *msg;
     has_utm_reading1_ = true;
     break;
+    //timer_ = ros::Time::now();
+    //while((ros::Time::now() - timer_).toSec() < 2){
+ //     mean_filter_.add(*msg);
+   // }
+    //utm_reading1_ = mean_filter_.getFilteredValue();
+    //has_utm_reading1_ = true;
+    //break;
   case states::S2_MOVING:
     ROS_INFO("moving....");
     if(!isMoving_)
@@ -113,7 +136,44 @@ void PoseEstimator::gpsOdomCallback(const nav_msgs::Odometry::ConstPtr& msg)
 }
 
 void PoseEstimator::odomP3atCallback(const nav_msgs::Odometry::ConstPtr& msg){
-  odom_p3at_.pose.pose.position.x = msg->pose.pose.position.x;
+
+  if(!has_odom_initial_)
+  {
+    odom_initial_.pose.pose.position.x = msg->pose.pose.position.x;
+    has_odom_initial_ = true;
+  }
+
+  odom_p3at_.pose.pose.position.x = msg->pose.pose.position.x - odom_initial_.pose.pose.position.x;
+
+
+
+}
+
+void PoseEstimator::imuCallback(const sensor_msgs::Imu::ConstPtr& msg){
+  if(!has_imu_initial_)
+  {
+    imu_initial_ = *msg;
+    has_imu_initial_ = true;
+    quat_initial_.setX(msg->orientation.x);
+    quat_initial_.setY(msg->orientation.y);
+    quat_initial_.setZ(msg->orientation.z);
+    quat_initial_.setW(msg->orientation.w);
+
+    yaw_initial_ = tf::getYaw(quat_initial_);
+    std::cout << "yaw initial: " << yaw_initial_ << std::endl;
+  }
+  imu_data_ = *msg;
+  quat_data_.setX(msg->orientation.x);
+  quat_data_.setY(msg->orientation.y);
+  quat_data_.setZ(msg->orientation.z);
+  quat_data_.setW(msg->orientation.w);
+  yaw_data_ = tf::getYaw(quat_data_);
+  std::cout << "yaw data: " << yaw_data_ << std::endl;
+}
+
+
+void PoseEstimator::initialPoseCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg){
+
 
 }
 
